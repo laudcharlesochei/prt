01f1e50649c55a7f24fa4667f23d1a6c5be9b73e
Laud Charles Ochei <>
2021-06-10 10:21:00 +0100

    Merge origin/main

---
+++ src/test/java/com/prt/PrtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
